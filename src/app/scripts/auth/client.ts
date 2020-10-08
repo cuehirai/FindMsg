70a5a3b640811c5b409a6bf557b3a40ad8c8f2ed
cuehirai <72176464+cuehirai@users.noreply.github.com>
2020-10-08 13:15:17 +0900

    modify1

--- src/app/scripts/auth/client.ts
+++ src/app/scripts/auth/client.ts
@@ -1,6 +1,6 @@
 import { PublicClientApplication } from "@azure/msal-browser";
 
-const appId = "67dc74f0-fb8c-47a4-8435-63253a3fc3bb"; // FindMsg
+const appId = "2e9f73d3-65f6-4ed6-8fbb-6ec025d9cde7"; // FindMsg
 // const appId = "8fe75eb2-476b-43e1-8e36-a09cf42fcb42"; // FindMsg-NoConsentTest
 
 export const client = new PublicClientApplication({
